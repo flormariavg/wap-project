07b6a4b3c9e765be7793f78dae7e91a61627b7bc
flormariavg <flormariavg@gmail.com>
2019-03-18 13:30:09 -0500

    mvc

--- book-store/src/edu/mum/wap/controller/LoginController.java
+++ book-store/src/edu/mum/wap/controller/LoginController.java
@@ -107,7 +107,7 @@ public class LoginController extends HttpServlet {
 			}else {
 			session.setAttribute("loggedIn", false);
             out.print("sorry, username or password error!");  
-            request.getRequestDispatcher("views/login.jsp").include(request, response); 
+            request.getRequestDispatcher("login.jsp").include(request, response); 
 //            request.getRequestDispatcher("login.jsp").forward(request, response);
 	          
 	        out.close();  
